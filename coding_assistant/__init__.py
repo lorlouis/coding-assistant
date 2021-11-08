671770be416c06500b3f602d92b1ed9ca06b8a4d
Edward Emmett <edemms12@gmail.com>
2021-11-08 18:03:36 -0500

    A rudimentary attempt to scale the size of the chat bubble.

--- coding_assistant/__init__.py
+++ coding_assistant/__init__.py
@@ -4,7 +4,7 @@ import sys
 _old_hook = sys.excepthook
 
 # I don't want to be sued my MS so I'll use a single 'p'
-clipy = """\\_______________________________/
+clipy = """
  _-_  | /
 /_  \\ |/
 (o)(o)
@@ -14,9 +14,11 @@ clipy = """\\_______________________________/
  ¯--¯"""
 
 
-def assistant_print(type_, value, traceback):
-    _old_hook(type_, value, traceback)
-    print(clipy)
+def assistant_print(type_, value, tb):
+    _old_hook(type_, value, tb)
+    width = len(f"{type_.__qualname__}{f': {value}' if value else ''}")-2
+    print(f"\\{'_'*width}/" + clipy)
+
 
 
 sys.excepthook = assistant_print
